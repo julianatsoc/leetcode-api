5df47ae8c9b6b8a1e83de8dc6776032f696af559
Juliana <julianatsoc28@gmail.com>
2025-02-26 01:33:22 -0300

    chore: criando servidor

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
