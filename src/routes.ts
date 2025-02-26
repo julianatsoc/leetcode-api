7ecfe557d99e9b02981e3ebc6aaa4bd8da6994ec
Juliana <julianatsoc28@gmail.com>
2025-02-26 01:37:43 -0300

    feat: adiciona rota de busca de usuário

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { getStats } from "./services";
+
+const router = Router();
+
+router.get("/status/:username", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const stats = await getStats(username);
+    res.json(stats);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar dados do usuário." });
+  }
+});
+
+export default router;
